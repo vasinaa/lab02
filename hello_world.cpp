3a7e5483adcde9beb393e008d74127031eb1dd6c
vasinaa <avasinaa15@yandex.ru>
2022-05-01 13:04:28 +0300

    comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
-int main () { std::string name; 
-std::cin >> name;
-std::cout << " Hello World from " << name << std::endl;
 
+int main() 
+{
+ std::string name; // creating the name
+ std::cin >> name; // enter name
+ std::cout << "Hello World from " << name << std::endl ; // print
 }
