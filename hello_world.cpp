aa2412599044d1af233963876657c7c5ec5a5f9b
vasinaa <avasinaa15@yandex.ru>
2022-05-01 12:30:35 +0300

    hello_world version 2

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main()
-{ cout<< "Hello World" << endl;}
-
+int main() { string name; cin >> name; cout << "Hello World from " << name << endl;}
